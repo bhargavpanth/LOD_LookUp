dc9e500903a2ab5151d01a156e2c357ae694ef5b
Bhargav Panth <bhargavrpanth@gmail.com>
2018-07-05 15:14:28 +0100

    read cache file

--- src/caching/prefix-tag-generator.py
+++ src/caching/prefix-tag-generator.py
@@ -1,4 +1,23 @@
 import urllib
 from bs4 import BeautifulSoup
+import json
 
 # http://lov.okfn.org/dataset/lov/vocabs/
+
+prefix_list = list()
+
+try:
+    with open('./cache/prefix_dump.json') as prefix:
+        json_prefix_file = json.load(prefix)
+except Exception as e:
+    raise
+    print 'prefix_dump.json file does not exist. Run prefix-extractor.py first to generate the prefix_dump.json file'
+else:
+    prefix_set = json_prefix_file["prefix"]
+    for each_prefix in prefix_set:
+        prefix_list.append(each_prefix)
+
+def request(url):
+    return urllib.urlopen(url).read()
+
+# print prefix_list
