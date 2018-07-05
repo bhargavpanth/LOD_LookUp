e1fcb94baec379cbfc14c3209c70941c8867bc83
Bhargav Panth <bhargavrpanth@gmail.com>
2018-07-05 16:08:36 +0100

    prefixes and tags caching

--- src/caching/prefix-tag-generator.py
+++ src/caching/prefix-tag-generator.py
@@ -1,10 +1,12 @@
 import urllib
 from bs4 import BeautifulSoup
 import json
+import io
 
 # http://lov.okfn.org/dataset/lov/vocabs/
 
 prefix_list = list()
+dump_list = list()
 
 try:
     with open('./cache/prefix_dump.json') as prefix:
@@ -22,6 +24,7 @@ def request(url):
 
 def extract(page, pf):
     tags = list()
+    global dump_list
     try:
         soup = BeautifulSoup(page, 'html.parser')
     except Exception as e:
@@ -29,16 +32,35 @@ def extract(page, pf):
     else:
         try:
             for each_tag in soup.findAll('a', {'class': 'tag'}):
-                tags.append(each_tag)
+                tags.append(each_tag.getText())
         except Exception as e:
             print 'no tags found for vocab', pf
+    data = {'prefix': pf, 'tags': tags}
+    dump_list.append(data)
+
+
+def json_dump():
+    global dump_list
+    with open('./cache/prefix-tag_dump.json', 'w+') as tags_dump:
+        dump = json.dumps(dump_list, indent=4, sort_keys=True,
+                              separators=(',', ': '), ensure_ascii=False)
+        tags_dump.write(dump)
+        print 'prefix and tag pair dumped'
 
 def main():
+    count = 1
     global prefix_list
     url = 'http://lov.okfn.org/dataset/lov/vocabs/'
     for each_prefix in prefix_list:
+        print 'prefix ', count, ' of', len(prefix_list), ' processing...'
         _url = url + str(each_prefix)
         content = request(_url)
         extract(content, str(each_prefix))
+        count += 1
+    print 'extraction of all prefixes and tags completed... Dumping the data in cache'
+    json_dump()
+
+if __name__ == '__main__':
+    main()
 
 # print prefix_list
