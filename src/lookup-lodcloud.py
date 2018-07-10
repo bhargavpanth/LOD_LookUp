0122c4f392fe029f226e41e46d6ad7534322fa7f
Bhargav Panth <bhargavrpanth@gmail.com>
2018-07-10 13:53:37 +0100

    lookup for tags

--- src/lookup-lodcloud.py
+++ src/lookup-lodcloud.py
@@ -8,18 +8,23 @@ from matplotlib.gridspec import GridSpec
 
 def plot(vocab_list, open_vocab, close_vocab, total_vocab):
     # labels = vocab_list
+    vocabs = list()
+    for each_vocab in vocab_list:
+        vocabs.append(each_vocab)
     fracs = [open_vocab, close_vocab]
-    explode = (0, 0.05, 0, 0)
+    labels = ['close', 'open']
+    # explode = (0, 0.05, 0, 0)
     # Make square figures and axes
     the_grid = GridSpec(2, 2)
     plt.subplot(the_grid[0, 0], aspect=1)
-    plt.pie(fracs, autopct='%1.1f%%', shadow=True)
+    plt.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True)
     plt.subplot(the_grid[0, 1], aspect=1)
     plt.show()
 
 
 def get_vocab_ratio(dataset):
     vocab_list = list()
+    # tag_list = list()
     # given a dataset -> export the vocab list
     try:
         client = MongoClient('localhost', 27017)
@@ -28,14 +33,23 @@ def get_vocab_ratio(dataset):
     else:
         db = client['lodcloud']
         ds = db.vocab.find({'dataset': dataset})
-        # ds = db.domain_vocab.find({'dataset': dataset})
         for each_vocab in ds:
             vocab_list.append(each_vocab['vocab'])
-            # print each_vocab['tag']
-        ratio_of_open_to_close_vocab(vocab_list, dataset)
+        tag_list = get_tags(dataset)
+        ratio_of_open_to_close_vocab(vocab_list, dataset, tag_list)
+
+def get_tags(dataset):
+    tag_list = list()
+    client = MongoClient('localhost', 27017)
+    db = client['lodcloud']
+    dbs = db.vocab_domain.find({'dataset': dataset})
+    for each_tag in dbs:
+        print each_tag['tag']
+        tag_list.append(each_tag['tag'])
+    return tag_list
 
 
-def ratio_of_open_to_close_vocab(vocab_list, dataset):
+def ratio_of_open_to_close_vocab(vocab_list, dataset, tags):
     vocab_set = set()
     
     open_vocab_count = 0
@@ -56,6 +70,7 @@ def ratio_of_open_to_close_vocab(vocab_list, dataset):
     print 'Open vocabs : ', open_vocab_count
     print 'Closed vocabs : ', closed_vocab_count
     print 'Total Vocabs : ', total_vocabs
+    print 'Vocab Tags : ', tags
     print '-+-+-+-+-+-+-+-+-+-+-+-+-+-+-'
     plot(vocab_list, open_vocab_count, closed_vocab_count, total_vocabs)
 
